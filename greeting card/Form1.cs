db458e1dae4754a4505d5af07c37bae1f32b40aa
jamekoch117 <jamekoch117@SNS-D-130RZZ>
2019-10-03 13:02:37 -0400

    Final changes

--- greeting card/Form1.cs
+++ greeting card/Form1.cs
@@ -34,9 +34,9 @@ namespace greeting_card
             g.DrawString("for free v bucks ", titlefont1, titlebrush, 300, 165);
             Font titlefont2 = new Font("Times New Roman", 18);
             g.DrawString("Not a scam", titlefont2, titlebrush, 395, 195);
-
             g.DrawRectangle(drawPen, 210, 100, 350, 150);
             //the rectangles
+
             Pen drawPen1 = new Pen(Color.DarkOliveGreen, 25);
             g.DrawRectangle(drawPen1, 135, 46, 495, 255);
             Pen drawPen2 = new Pen(Color.LightGoldenrodYellow, 25);
@@ -49,11 +49,13 @@ namespace greeting_card
         }
         private void Form1_Click(object sender, EventArgs e)
         {//the commands 
+
             Graphics g = this.CreateGraphics();
             Pen tilepen = new Pen(Color.Black);
             Font titlefont = new Font("Courier New", 18);
             Font smallfont = new Font("Courier New", 8);
             //you have been scammed writing
+
             SoundPlayer mac = new SoundPlayer(Properties.Resources.mac);
             mac.Play();
             SolidBrush titlebrush = new SolidBrush(Color.AntiqueWhite);
@@ -70,6 +72,7 @@ namespace greeting_card
             Thread.Sleep(500);
             g.DrawString("fooled", titlefont, titlebrush3, 280, 180);
             Thread.Sleep(500);
+
            //clearing screen and putting flashing colours 
             g.Clear(Color.White);
             Thread.Sleep(100);
@@ -90,26 +93,33 @@ namespace greeting_card
             g.Clear(Color.White);
             Thread.Sleep(100);
             g.Clear(Color.Black);
+
             //the road
             g.DrawRectangle(drawPen, 0, 180, 900, 200);
             g.FillRectangle(titlebrush, 0, 180, 900, 200);
             //linedivider 
-            Pen drawPen2 = new Pen(Color.Black, 15);
-            g.DrawLine(drawPen2, 10, 270, 120, 270);
-            g.DrawLine(drawPen2, 230, 270, 340, 270);
-            g.DrawLine(drawPen2, 450, 270, 560, 270);
-            g.DrawLine(drawPen2, 670, 270, 780, 270);
+
+            Pen blackPen = new Pen(Color.Black, 15);
+            g.DrawLine(blackPen, 10, 270, 120, 270);
+            g.DrawLine(blackPen, 230, 270, 340, 270);
+            g.DrawLine(blackPen, 450, 270, 560, 270);
+            g.DrawLine(blackPen, 670, 270, 780, 270);
             //sun
+
             Pen drawPen3 = new Pen(Color.Yellow, 15);
             SolidBrush titleBrush1 = new SolidBrush(Color.LightYellow);
             g.FillEllipse(titleBrush1, 10, 10, 80, 90);
             //lines falling
+
             SolidBrush titlebrush4 = new SolidBrush(Color.Black);
             g.DrawString("Get hacked", titlefont, titlebrush4, 150, 0);
             Thread.Sleep(100);
             SolidBrush titlebrush5 = new SolidBrush(Color.White);
             SoundPlayer fall = new SoundPlayer(Properties.Resources.fall);
             fall.Play();
+
+            //get hacked
+
             g.DrawString("Get hacked", titlefont, titlebrush5, 150, 0);
             Thread.Sleep(10);
             g.DrawString("Get hacked", titlefont, titlebrush5, 150, 15);
